8c4960c60e65b7cc5fb73a0e7e716a327d2e22c7
tobyburkey <tobyburkey@10.30.11.158>
2019-04-23 10:52:40 -0400

    updates to main

--- src/dfa/Main.java
+++ src/dfa/Main.java
@@ -22,7 +22,7 @@ public class Main {
                 System.out.println("File not found.");
             }
         } else { // get file from input
-            Scanner scanner = new Scanner(System.in);
+           Scanner scanner = new Scanner(System.in);
             while (fileAccepted == false) {
                 System.out.println("Please enter the Controller Description file name: ");
                 String input = scanner.next();
@@ -39,17 +39,30 @@ public class Main {
         }
 
         // print description
-//        c.printDescription();
-
+        //c.printDescription();
         // Get test string
-        System.out.print("Enter a string to test: ");
-        while(true){
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.next();
-
-            // run Controller on text string
-            c.testInput(input);
-            System.out.print("\nEnter another string to test: ");
-        }
+       System.out.print("Enter a string to test: ");
+       Scanner testInput = new Scanner(System.in);
+       while(true){
+    	  // Scanner testInput = new Scanner(System.in);
+    	   String input = testInput.next();
+           //run Controller on text string
+          c.testInput(input);
+           
+    	   System.out.println("Do you have any more inputs to test? (y/n)");
+    	   Scanner moreTests = new Scanner(System.in);
+    	   moreTests.next();
+    	   String moreInputs = moreTests.next();
+    	   while(moreInputs=="y") {
+    		   System.out.print("\nEnter another string to test: ");
+    		   Scanner anotherTest = new Scanner(System.in);
+    		  // input = anotherTest.next();
+    		 //  c.testInput(input);
+    	   }
+    	   if(moreInputs=="n") {
+    		   break;
+    	   }
+       }
+    //  c.testInput("0100000001");
     }
 }
