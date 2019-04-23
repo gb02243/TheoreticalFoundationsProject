2e48d9dbcf35233830cbba6449cbe016f6e97f24
griffin <gb02243@georgiasouthern.edu>
2019-04-23 11:02:16 -0400

    Fixed Error with Scanner

--- src/dfa/Main.java
+++ src/dfa/Main.java
@@ -10,6 +10,7 @@ public class Main {
         String filename;
         File file;
         Controller c = null;
+        Scanner scanner;
 
         // get file from argument
         if (args.length == 1) {
@@ -22,7 +23,7 @@ public class Main {
                 System.out.println("File not found.");
             }
         } else { // get file from input
-           Scanner scanner = new Scanner(System.in);
+           scanner = new Scanner(System.in);
             while (fileAccepted == false) {
                 System.out.println("Please enter the Controller Description file name: ");
                 String input = scanner.next();
@@ -30,7 +31,6 @@ public class Main {
                 file = new File(filename);
                 if (file.isFile()) {
                     c = new Controller(file);
-                    scanner.close();
                     fileAccepted = true;
                 } else {
                     System.out.println("File not found.");
@@ -40,29 +40,14 @@ public class Main {
 
         // print description
         //c.printDescription();
+
         // Get test string
-       System.out.print("Enter a string to test: ");
-       Scanner testInput = new Scanner(System.in);
-       while(true){
-    	  // Scanner testInput = new Scanner(System.in);
-    	   String input = testInput.next();
-           //run Controller on text string
-          c.testInput(input);
-           
-    	   System.out.println("Do you have any more inputs to test? (y/n)");
-    	   Scanner moreTests = new Scanner(System.in);
-    	   moreTests.next();
-    	   String moreInputs = moreTests.next();
-    	   while(moreInputs=="y") {
-    		   System.out.print("\nEnter another string to test: ");
-    		   Scanner anotherTest = new Scanner(System.in);
-    		  // input = anotherTest.next();
-    		 //  c.testInput(input);
-    	   }
-    	   if(moreInputs=="n") {
-    		   break;
-    	   }
-       }
-    //  c.testInput("0100000001");
+        System.out.println("Enter a string to test: ");
+        while (true) {
+            scanner = new Scanner(System.in);
+            String input = scanner.next();
+            c.testInput(input);
+            System.out.println("Enter another string to test:");
+        }
     }
 }
